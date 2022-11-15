b6555c1af513606bc8f6519dc5f0f5d7f8f4abf7
jihan-ice <116784192+jihan-ice@users.noreply.github.com>
2022-11-15 21:38:33 +0800

    Add files via upload

---
+++ js/newlayers/money.js
@@ -0,0 +1,25 @@
+addLayer("money", { //这是代码中的节点代码 例如player.p可以调用该层级的数据 尽量使用顺手的字母什么的 不建议数字开头
+    symbol: "M", // 这是节点上显示的字母
+    position: 1, // 节点顺序
+    startData() { return {
+        unlocked: true, //是否开始就解锁
+		points: new ExpantaNum(0),
+    }},
+    color: "Gold",
+    resource: "金币", // 重置获得的资源名称
+    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    requires:new ExpantaNum(10),
+    exponent:1,
+    baseAmount(){return player.points},//基础资源数量
+    baseResource:"点数",//基础资源名称
+    gainMult() { // 资源获取数量倍率
+        mult = new ExpantaNum(1)
+        return mult
+    },
+    gainExp() { // 资源获取指数加成(与exponent相乘)
+        var exp = new ExpantaNum(1)
+        return exp
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)  QwQ:1也可以当第一排
+    layerShown(){return player.wf.gd.gte(n(1e20)},
+})
