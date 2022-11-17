11720440a8a1f33870e7b8ee9d718106b6bb3b2f
jihan-ice <116784192+jihan-ice@users.noreply.github.com>
2022-11-17 18:37:40 +0800

    update layers.js

--- js/layers.js
+++ js/layers.js
@@ -254,20 +254,20 @@ addLayer("wf", { //这是代码中的节点代码 例如player.p可以调用该�
       },
     },
 })
-addLayer("money", { //这是代码中的节点代码 例如player.p可以调用该层级的数据 尽量使用顺手的字母什么的 不建议数字开头
-    symbol: "M", // 这是节点上显示的字母
-    position: 1, // 节点顺序
+addLayer("p", { //这是代码中的节点代码 例如player.p可以调用该层级的数据 尽量使用顺手的字母什么的 不建议数字开头
+    symbol: "P", // 这是节点上显示的字母
+    position: 0, // 节点顺序
     startData() { return {
-        unlocked: false, //是否开始就解锁
+        unlocked: true, //是否开始就解锁
 		points: new ExpantaNum(0),
     }},
-    color: "Gold",
-    resource: "金币", // 重置获得的资源名称
+    color: "lime",
+    resource: "重置点", // 重置获得的资源名称
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    requires:new ExpantaNum(1e20),
+    requires:new ExpantaNum(10),
     exponent:1,
-    baseAmount(){return player.wf.gd},//基础资源数量
-    baseResource:"功德",//基础资源名称
+    baseAmount(){return player.points},//基础资源数量
+    baseResource:"点数",//基础资源名称
     gainMult() { // 资源获取数量倍率
         mult = new ExpantaNum(1)
         return mult
