f4824e4f579d28018a5d6a9be5b7815e9ab3087c
jihan-ice <116784192+jihan-ice@users.noreply.github.com>
2022-11-17 12:41:55 +0800

    update money.js

--- js/newlayers/money.js
+++ js/newlayers/money.js
@@ -23,6 +23,6 @@ addLayer("money", { //这是代码中的节点代码 例如player.p可以调用�
     row: 1, // Row the layer is in on the tree (0 is the first row)  QwQ:1也可以当第一排
     layerShown(){
       if(player.wf.gd.gte(1e20))return true
-      else return false
+      return false
     },
 })
