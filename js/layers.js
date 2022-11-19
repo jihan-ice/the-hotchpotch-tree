4eb8f96b4e70125e3e0130bb7e88c6367371dcd0
jihan-ice <116784192+jihan-ice@users.noreply.github.com>
2022-11-19 20:17:30 +0800

    update layers.js

--- js/layers.js
+++ js/layers.js
@@ -287,9 +287,9 @@ addLayer("money", { //这是代码中的节点代码 例如player.p可以调用�
       cost: n(1),
       effect(){
         var eff=n(1)
-        if(hasUpgrade("money",11))
-        eff=eff.add(player.money.points.add(1).pow(0.5))
-        else return eff
+        if(hasUpgrade("money",11))eff = eff.add(player.money.points.add(1).pow(0.5))
+        return eff
+      },
       },
       effectDisplay(){return `x${format(this.effect())}`}
                 },
